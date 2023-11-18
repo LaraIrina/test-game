bb8483012270c99c3c6d9c072c88049f069962e0
LaraIrina <89276310+LaraIrina@users.noreply.github.com>
2023-11-19 00:07:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,6 @@ class Player {
         //only let gravity hit when player is on top position
         if (this.position.y + this.height +this.velocity.y <= canvas.height)
             this.velocity.y += gravity //falling down faster the closer to ground
-        else this.velocity.y = 0 //when player hits bottom the condition becomes falls and velocity is set to 0 so it stops moving
     }
 }
 
@@ -71,44 +70,53 @@ class Platform {
 
 // Platform generic objects aka background 
 class GenericObjects {
-    constructor({ x, y, image, width, height }) {
+    constructor({ x, y, image}) {
         this.position = {
             x,
             y
         }
         this.image = image
-        this.width = width
-        this.height = height
+        this.width = image.width
+        this.height = image.height
     }
     draw() {
         //c.fillStyle = "blue" //set color of player
         //c.fillRect(this.position.x, this.position.y, this.width, this.height)
-        c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
+        c.drawImage(this.image, this.position.x, this.position.y)
     }
+}
+//Function to create images
+function createImage(imageSrc){
+    const image = new Image()
+    image.src = imageSrc
+    return image
 }
 
 //Create Platform
-const platform_offset = 20
-const image = new Image()
-image.src = "./img/platform.png"
-const platforms = [
-    new Platform({x: -10, y: window.innerHeight - window.innerHeight * 0.1 - platform_offset, image:image, width: 500, height:150}),
-    new Platform({x: 480, y: window.innerHeight - window.innerHeight * 0.1 - platform_offset, image:image, width: 500, height:150})
+let platform_offset = 30
+let platformImageSrc = "./img/platform-small.png"
+let platformImage = createImage(platformImageSrc)
+
+let platforms = [
+    new Platform({x: -10, y: 470+platform_offset, image:platformImage, width: 500, height:150}),
+    new Platform({x: 470, y: 470+platform_offset, image:platformImage, width: 500, height:150}),
 ];
 //Create Character
-const character = new Image()
-character.src = "./img/character.png"
-const player = new Player({x: -10, y: 470, image:character, width: 320, height:180})
+let characterImageSrc = "./img/character.png"
+let player = new Player({x: -10, y: 470, image:createImage(characterImageSrc), width: 320, height:180})
 
 //Create Background
-const background_img = new Image()
-background_img.src = "./img/background.png"
-const background = new GenericObjects({
-    x: 0,
-    y: 0,
-    image: background_img,
-    width: canvas.width,
-    height: canvas.height
+let backgroundImageSrc = "./img/background-3.png"
+let background = new GenericObjects({
+    x: -1,
+    y: -1,
+    image: createImage(backgroundImageSrc)
+})
+let hillsImageSrc = "./img/hills.png"
+let hills = new GenericObjects({
+    x: -1,
+    y: -1,
+    image: createImage(hillsImageSrc)
 })
 
 //Constant for key presses
@@ -122,14 +130,49 @@ const keys = {
 }
 //Create Win Scenario: As soon as player moved a certain amount of pixels
 let scrollOffset = 0
-const pixelsToWin = 2000
+let pixelsToWin = 2000
 
+//Function to reset game when player falls in death pit
+function init(){
+
+//Create Platform
+ platform_offset = 30
+ platformImageSrc = "./img/platform-small.png"
+ platformImage = createImage(platformImageSrc)
+
+ platforms = [
+    new Platform({x: -10, y: 470+platform_offset, image:platformImage, width: 500, height:150}),
+    new Platform({x: 470, y: 470+platform_offset, image:platformImage, width: 500, height:150}),
+];
+//Create Character
+ characterImageSrc = "./img/character.png"
+ player = new Player({x: -10, y: 470, image:createImage(characterImageSrc), width: 320, height:180})
+
+//Create Background
+ backgroundImageSrc = "./img/background-3.png"
+ background = new GenericObjects({
+    x: -1,
+    y: -1,
+    image: createImage(backgroundImageSrc)
+})
+ hillsImageSrc = "./img/hills.png"
+ hills = new GenericObjects({
+    x: -1,
+    y: -1,
+    image: createImage(hillsImageSrc)
+})
+
+//Create Win Scenario: As soon as player moved a certain amount of pixels
+ scrollOffset = 0
+ pixelsToWin = 2000
+}
 //Function to add animation to game
 function animate(){
     requestAnimationFrame(animate) //calls animate function over and over again
     c.fillStyle = "white"
     c.fillRect(0, 0 ,canvas.width, canvas.height) //clears canvas and removes 'old' player so that it doesn't draw a line after itself
     background.draw()
+    hills.draw()
 
     platforms.forEach(platform => {
         platform.draw() //draw platform
@@ -146,31 +189,45 @@ function animate(){
     //Monitor edges where player doesn't move anymore but platforms do
     else{
         player.velocity.x = 0 //stop moving when key is released
+
         if(keys.right.pressed){
             platforms.forEach(platform => {
                 platform.position.x -= 5 //move platform to left instead of player to right
                 scrollOffset += 5 //add pixels to the moved pixsels for win
             })
+            background.position.x -= 3 //slower to create a parallax scroll
+            hills.position.x -= 3 //slower to create a parallax scroll
+
         }
         else if(keys.left.pressed){
             platforms.forEach(platform => {
                 platform.position.x += 5 //move platform to left instead of player to right
                 scrollOffset -= 5 //remove pixels to the moved pixsels for win
-            })        
+            })  
+            background.position.x += 3 //slower to create a parallax scroll
+            hills.position.x += 3 //slower to create a parallax scroll
+
+      
         }
     } 
     
 //Collision Detection with Platform
     const offset_x = 150 //to make up for characters width
-    const offset_y = 45 //to make up for characters height
+    const offset_y = 60 //to make up for characters height
     platforms.forEach(platform => {
     if (
         player.position.y + player.height <= platform.position.y+offset_y && player.position.y + player.height + player.velocity.y >= platform.position.y+offset_y && player.position.x + player.width >= platform.position.x+offset_x && player.position.x <= platform.position.x-offset_x + platform.width){
         player.velocity.y =  0 // stop moving when collision detected and player hits 'floor'
     }})
+//Condition to win
     if(scrollOffset > pixelsToWin){
         console.log("You win!")
     }
+//Condition to loose
+if(player.position.y > canvas.height){
+    console.log("You loose!")
+    init() //reset everything
+}
 }
 animate() //call animate function
 
